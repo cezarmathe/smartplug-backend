efcf231c7d03e93c7e37708c40d1ca5bcc53294c
cezarmathe <cezarmathe@gmail.com>
2018-07-08 02:28:06 +0300

    worked on the post request handling and mqtt helper

--- src/app.py
+++ src/app.py
@@ -1,7 +1,51 @@
-import helpers.firebase_helper as fb
+from flask import Flask
+from flask import request
+# from flask import render_template
+import helpers.firebase_helper as fcm
 import helpers.mqtt_helper as mqtt
+from threading import Thread
+from time import sleep
 
-fb.print_result()
+def runMqtt():
+    print("[MQTT]--Thread started")
+    mqtt.init()
+    mqtt.client.loop_forever()
+    # while True:
+    #     sleep(50)
+    #     v = mqtt.getMessage()
+    #     if v == 0:
+    #         continue
+    #     print(v["topic"] + "->" + v["payload"])
 
-mqtt.mqqt_connect()
-mqtt.client.loop_forever()
+app = Flask(__name__)
+
+@app.route('/upstream', methods=['POST'])
+def handleUpstream():
+    type = request.form['type']
+    if type == 'registration_id':
+        return
+    if type == 'status_change':
+        return
+    return 'unknown type'
+
+def main():
+    mqttThread = Thread(target = runMqtt)
+    mqttThread.start()
+    # thread.join()
+    app.run(host='192.168.0.157', port='1234')
+
+if (__name__ == "__main__"):
+    main()
+
+
+
+
+
+
+
+
+
+# fb.print_result()
+#
+# mqtt.mqqt_connect()
+# mqtt.client.loop_forever()
--- src/helpers/mqtt_helper.py
+++ src/helpers/mqtt_helper.py
@@ -1,37 +1,85 @@
 import paho.mqtt.client as mqtt
 import helpers.secret_parser as secret
 
+# All the topics that the client will subscribe to
+topics = ["test", "conn_status", "status_updates"]
 
-# The callback for when the client receives a CONNACK response from the server
-def on_connect(client, userdata, flags, rc):
-    # Printing the result code for debugging
-    print("Connected with result code " + str(rc))
-
-    # Subscribing to all the topics in on_connect in order
-    # to resubscribe if the connection is lost
-    client.subscribe([("test", 0), ("conn_status", 0), ("status_updates", 0)])
-
-# The callback used for interpreting received messages
-def on_message(client, userdata, msg):
-    print("Recevied message: topic:" + msg.topic + " - payload:" + str(msg.payload)[1:])
+# The MQTT message buffer
+# global bufferStatusTopic
+# global bufferStatusPayload
+# global lastItem
 
 # Creating the client
 client = mqtt.Client("server", False)
 
-# Assigning the callbacks
-client.on_connect = on_connect
-client.on_message = on_message
+# CALLBACKS
+# The callback for when the client receives a CONNACK response from the server
+def onConnect(client, userdata, flags, rc):
+    # Printing the result code for debugging
+    print("[MQTT]--Connected with result code " + str(rc))
 
-# Parsing connection and authentication details from json
-creds = secret.retrieve('mqtt')
+    # Subscribing to all the topics in on_connect in order
+    # to resubscribe if the connection is lost
+    for i in topics:
+        client.subscribe(i, 0)
 
-# Setting up the credentials
-client.username_pw_set(creds["username"], creds["password"])
-
-# Connect to the server
-def mqqt_connect():
-    client.connect(creds["host"], creds["port"], 60)
-    client.publish("test", "succesfully connected")
-
-def publish(topic, payload):
+    print("[MQTT]--Succefsully subscribed to all the topics")
     return
+
+# The callback used for interpreting received messages
+def onMessage(client, userdata, msg):
+    # Extracting the topic and the payload
+    topic = str(msg.topic)
+    payload = str(msg.payload)
+    payload = payload[2 : len(payload) - 1]
+    # updateBuffer(topic, payload)
+
+    print("[MQTT]--Recevied message:" + topic + " - payload:" + payload)
+    return
+
+# Initialize MQTT
+def init():
+    # lastItem = -1
+    # bufferStatusTopic = list()
+    # bufferStatusPayload = list()
+    # Assigning the callbacks
+    client.on_connect = onConnect
+    client.on_message = onMessage
+    # Parsing connection and authentication details from json
+    creds = secret.retrieve('mqtt')
+    # Setting up the credentials
+    client.username_pw_set(creds["username"], creds["password"])
+    # Connecting
+    client.connect(creds["host"], creds["port"], 60)
+
+    print("[MQTT]--Initialized")
+    return
+
+# Simple publish function
+def publish(topic, payload):
+    client.publish(topic, payload)
+
+    print("[MQTT]Published a message to the topic(" + topic + ") with the payload(" + topic + ")")
+    return
+
+# def updateBuffer(topic, payload):
+#     # Updating the buffer
+#     lastItem += 1
+#     bufferStatusTopic.append(topic)
+#     bufferStatusPayload.append(payload)
+
+# Retrieve a message from the bufferStatus
+# def getMessage():
+#     # Checking the bufferStatus
+#     # If the bufferStatus is empty, returns 0
+#     if lastItem == -1:
+#         return lastItem
+#     # Otherwise, creating a message as a dictionary
+#     message = {"topic" : bufferStatusTopic[0],
+#                "payload" : bufferStatusPayload[0]
+#     }
+#     # Then, we delete the message from the bufferStatus
+#     del bufferStatusTopic[0]
+#     del bufferStatusPayload[0]
+#     lastItem -= 1
+#     return message
