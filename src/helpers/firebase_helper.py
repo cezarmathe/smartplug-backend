b5796713b3168edae58c95d786af00f366c9dad3
cezarmathe <cezarmathe@gmail.com>
2018-07-08 15:36:18 +0300

    Worked a little on firebase

--- src/helpers/firebase_helper.py
+++ src/helpers/firebase_helper.py
@@ -1,14 +1,31 @@
 from pyfcm import FCMNotification
 import helpers.secret_parser as secret
 
+# Retrieve the creds
 secrets = secret.retrieve('firebase')
 
-push_service = FCMNotification(api_key=secrets["server-key"])
+# Create the message service
+messageService = FCMNotification(api_key=secrets["server-key"])
 
-registration_id = secrets["sender-id"]
-message_title = "Uber update"
-message_body = "Hi john, your customized news for today is ready"
-result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
+# Simple send data to id/ids function
+def sendData(id, payload, singleDevice = True):
+    if singleDevice:
+        result = messageService.single_device_data_message(registration_id=id, data_message=data_message)
+        print("[FIREBASE]--Sent data to single device:" + result)
+        return
+    result = messageService.multiple_devices_data_message(registration_ids=id, data_message=data_message)
+    print("[FIREBASE]--Sent data to multiple devices:" + result)
+    return
 
-def print_result():
-    print(result)
+# Simple send notification to id/ids function
+def sendNotification(id, title, body, singleDevice = True):
+    if singleDevice:
+        result = messageService.notify_single_device(registration_id=id, message_title=title, message_body=body)
+        print("[FIREBASE]--Sent notification to single device:" + result)
+        return
+    result = messageService.notifi_multiple_devices(registration_ids=id, message_title=title, message_body=body)
+    print("[FIREBASE]--Sent notification to multiple devices:" + result)
+    return
+
+def cleanRegistrationIds(ids):
+    return messageService.clean_registration_ids(ids)
